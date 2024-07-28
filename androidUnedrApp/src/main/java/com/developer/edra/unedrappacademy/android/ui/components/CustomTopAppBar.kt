7405703e2e717637ba53966baf2ff164fbf0d4e5
Erick David Rosario Alcantara <erickrosario08@outlook.com>
2024-07-27 23:38:14 -0400

    Se modifico el toolbar para tener m[as opciones en un menu

--- androidUnedrApp/src/main/java/com/developer/edra/unedrappacademy/android/ui/components/CustomTopAppBar.kt
+++ androidUnedrApp/src/main/java/com/developer/edra/unedrappacademy/android/ui/components/CustomTopAppBar.kt
@@ -1,11 +1,14 @@
 package com.developer.edra.unedrappacademy.android.ui.components
 
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.outlined.ExitToApp
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.outlined.Refresh
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,10 +17,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,6 +45,8 @@ fun CustomTopAppBar(
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
 
     val context = LocalContext.current
+
+    var menuExpanded by remember { mutableStateOf(false) }
 
     CenterAlignedTopAppBar(
         colors = TopAppBarDefaults.topAppBarColors(
@@ -62,28 +72,75 @@ fun CustomTopAppBar(
             }
         },
         actions = {
-            IconButton(onClick = {mainViewModel.triggerRefresh()  }) {
+            IconButton(onClick = { mainViewModel.triggerRefresh() }) {
                 Icon(
                     imageVector = Icons.Outlined.Refresh,
-                    contentDescription = "Refresh",
+                    contentDescription = stringResource(R.string.refresh),
                     tint = MaterialTheme.colorScheme.secondary
                 )
             }
-            IconButton(onClick = {
-                mainViewModel.logout { success ->
-                    if (success) {
-                        navHostController.navigate(NavScreen.WelcomeScreen.name)
-                    } else {
-                        Toast.makeText(context, "Error al Salir de tu cuenta", Toast.LENGTH_LONG).show()
-                    }
-                }
-            }) {
+            IconButton(onClick = { menuExpanded = !menuExpanded }) {
                 Icon(
-                    imageVector = Icons.AutoMirrored.Outlined.ExitToApp,
-                    contentDescription = "ExitApp",
+                    imageVector = Icons.Filled.MoreVert,
+                    contentDescription = stringResource(R.string.more_option),
                     tint = MaterialTheme.colorScheme.secondary
                 )
             }
+
+            DropdownMenu(
+                expanded = menuExpanded,
+                modifier = Modifier.background(MaterialTheme.colorScheme.secondary),
+                onDismissRequest = { menuExpanded = false },
+            ) {
+
+                DropdownMenuItem(
+                    text = {
+                        Text(stringResource(R.string.calendar_item_menu))
+                    },
+                    onClick = { /* TODO */ },
+                )
+                DropdownMenuItem(
+                    text = {
+                        Text(stringResource(R.string.program_student_item_menu))
+                    },
+                    onClick = { /* TODO */ },
+                )
+                DropdownMenuItem(
+                    text = {
+                        Text(stringResource(R.string.closet_session))
+                    },
+                    onClick = {
+                        mainViewModel.logout { success ->
+                            if (success) {
+                                navHostController.navigate(NavScreen.WelcomeScreen.name)
+                            } else {
+                                Toast.makeText(
+                                    context,
+                                    context.getString(R.string.error_message_logout),
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                    },
+                )
+            }
+
+
+//            IconButton(onClick = {
+//                mainViewModel.logout { success ->
+//                    if (success) {
+//                        navHostController.navigate(NavScreen.WelcomeScreen.name)
+//                    } else {
+//                        Toast.makeText(context, "Error al Salir de tu cuenta", Toast.LENGTH_LONG).show()
+//                    }
+//                }
+//            }) {
+//                Icon(
+//                    imageVector = Icons.AutoMirrored.Outlined.ExitToApp,
+//                    contentDescription = "ExitApp",
+//                    tint = MaterialTheme.colorScheme.secondary
+//                )
+//            }
         },
         scrollBehavior = scrollBehavior,
     )
